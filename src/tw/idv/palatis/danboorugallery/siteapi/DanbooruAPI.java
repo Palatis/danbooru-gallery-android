31d5b715f9e3949c79bb6a038777abb399b01193
Victor Tseng <palatis@iworldcom.com>
2011-08-31 05:49:19 +0800

    supports XML api

--- src/tw/idv/palatis/danboorugallery/siteapi/DanbooruAPI.java
+++ src/tw/idv/palatis/danboorugallery/siteapi/DanbooruAPI.java
@@ -7,7 +7,6 @@ import java.io.Reader;
 import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
 import java.io.Writer;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -15,20 +14,30 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.TreeSet;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
-import tw.idv.palatis.danboorugallery.defines.D;
 import tw.idv.palatis.danboorugallery.model.Post;
 import tw.idv.palatis.danboorugallery.model.Tag;
-import android.util.Log;
 
 public class DanbooruAPI
 	implements ISiteAPI
 {
 	public static final String	URL_POSTS_JSON	= "/post/index.json?page=%1$s&tags=%2$s&limit=%3$s";
 	public static final String	URL_TAGS_JSON	= "/tag/index.json?order=count&page=%1$s&name=*%2$s*&limit=%3$s";
+	public static final String	URL_POSTS_XML	= "/post/index.xml?page=%1$s&tags=%2$s&limit=%3$s";
+	public static final String	URL_TAGS_XML	= "/tag/index.xml?order=count&page=%1$s&name=*%2$s*&limit=%3$s";
 
 	private static final int	_BUFFER_SIZE	= 8192;
 
@@ -44,7 +53,7 @@ public class DanbooruAPI
 	public DanbooruAPI(String siteUrl)
 	{
 		mSiteUrl = siteUrl;
-		mApi = API_JSON;
+		mApi = API_XML;
 	}
 
 	public DanbooruAPI(String siteUrl, int api) throws UnsupportedAPIException
@@ -62,7 +71,7 @@ public class DanbooruAPI
 	@Override
 	public void setApi(int api) throws UnsupportedAPIException
 	{
-		if (api != API_JSON)
+		if (api != API_JSON || api != API_XML)
 			throw new UnsupportedAPIException( api );
 
 		mApi = api;
@@ -94,9 +103,9 @@ public class DanbooruAPI
 	public List < Post > fetchPostsIndex(int page, String tags, int limit)
 	{
 		if (mApi == API_JSON)
-			return fetchPostsIndexJSON( page, tags, limit );
+			return fetchPostsIndexJSON( page, tags.toLowerCase(), limit );
 		if (mApi == API_XML)
-			return fetchPostsIndexXML( page, tags, limit );
+			return fetchPostsIndexXML( page, tags.toLowerCase(), limit );
 		return null;
 	}
 
@@ -104,9 +113,9 @@ public class DanbooruAPI
 	public List < Tag > fetchTagsIndex(int page, String name, int limit)
 	{
 		if (mApi == API_JSON)
-			return fetchTagsIndexJSON( page, name, limit );
+			return fetchTagsIndexJSON( page, name.toLowerCase(), limit );
 		if (mApi == API_XML)
-			return fetchTagsIndexXML( page, name, limit );
+			return fetchTagsIndexXML( page, name.toLowerCase(), limit );
 		return null;
 	}
 
@@ -118,22 +127,8 @@ public class DanbooruAPI
 		try
 		{
 			fetchUrl = new URL( String.format( mSiteUrl + URL_POSTS_JSON, page, tags, limit ) );
-		}
-		catch (MalformedURLException e)
-		{
-			return null;
-		}
 
-		Log.v( D.LOGTAG, "DanbooruAPI::fetchPostsIndex(): fetching " + fetchUrl.toExternalForm() );
-
-		if (mIsCanceled)
-			return null;
-
-		Reader input;
-		try
-		{
-			input = new BufferedReader( new InputStreamReader( fetchUrl.openStream(), "UTF-8" ) );
-
+			Reader input = new BufferedReader( new InputStreamReader( fetchUrl.openStream(), "UTF-8" ) );
 			Writer output = new StringWriter();
 
 			int count = 0;
@@ -176,6 +171,51 @@ public class DanbooruAPI
 
 	private List < Post > fetchPostsIndexXML(int page, String tags, int limit)
 	{
+		mIsCanceled = false;
+
+		try
+		{
+			URL fetchUrl = new URL( String.format( mSiteUrl + URL_POSTS_XML, page, tags, limit ) );
+
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			Document doc = db.parse( new InputSource( fetchUrl.openStream() ) );
+			doc.getDocumentElement().normalize();
+
+			NodeList nodes = doc.getElementsByTagName( "post" );
+
+			int length = nodes.getLength();
+			ArrayList < Post > posts = new ArrayList < Post >( length );
+			for (int i = 0; i < length; ++i)
+			{
+				Node node = nodes.item( i );
+				posts.add( new Post( (Element) node ) );
+				if (mIsCanceled)
+					return null;
+			}
+			return posts;
+		}
+		catch (UnsupportedEncodingException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (SAXException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (ParserConfigurationException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		return null;
 	}
 
@@ -197,7 +237,6 @@ public class DanbooruAPI
 			try
 			{
 				URL fetchUrl = new URL( String.format( mSiteUrl + URL_TAGS_JSON, page, keyword, limit ) );
-				Log.d( D.LOGTAG, "query for tags: " + fetchUrl.toExternalForm() );
 
 				Reader input = new BufferedReader( new InputStreamReader( fetchUrl.openStream(), "UTF-8" ) );
 				Writer output = new StringWriter();
@@ -233,6 +272,51 @@ public class DanbooruAPI
 
 	private List < Tag > fetchTagsIndexXML(int page, String name, int limit)
 	{
-		return null;
+		TreeSet < Tag > ret = new TreeSet < Tag >( new Tag.CompareById() );
+		String keywords[] = new HashSet < String >( Arrays.asList( name.split( "\\+" ) ) ).toArray( new String[] { } );
+
+		for (String keyword : keywords)
+		{
+			// trim leading and trailing whitespace, then replace spaces
+			// with underscores.
+			// because typing underscore with soft-keyboard is a pain in the
+			// ass...
+			keyword = keyword.trim().replace( ' ', '_' );
+
+			// get data from network...
+			try
+			{
+				URL fetchUrl = new URL( String.format( mSiteUrl + URL_TAGS_XML, page, keyword, limit ) );
+
+				DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+				DocumentBuilder db = dbf.newDocumentBuilder();
+				Document doc = db.parse( new InputSource( fetchUrl.openStream() ) );
+				doc.getDocumentElement().normalize();
+
+				NodeList nodes = doc.getElementsByTagName( "tag" );
+
+				int length = nodes.getLength();
+				for (int i = 0; i < length; ++i)
+					ret.add( new Tag( (Element) nodes.item( i ) ) );
+			}
+			catch (UnsupportedEncodingException e)
+			{
+			}
+			catch (IOException e)
+			{
+			}
+			catch (SAXException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			catch (ParserConfigurationException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		return Arrays.asList( ret.toArray( new Tag[] { } ) );
 	}
 }
