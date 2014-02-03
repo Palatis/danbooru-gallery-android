00642d43aff6edc62d2c36a5413566f616703ece
Victor Tseng <palatis@gmail.com>
2014-02-03 16:00:22 +0800

    App: init NetworkChangeReceiver after Picasso

--- DanbooruGallery/src/main/java/tw/idv/palatis/danboorugallery/DanbooruGalleryApplication.java
+++ DanbooruGallery/src/main/java/tw/idv/palatis/danboorugallery/DanbooruGalleryApplication.java
@@ -51,10 +51,10 @@ public class DanbooruGalleryApplication
     public void onCreate()
     {
         super.onCreate();
-        NetworkChangeReceiver.init(this);
         DanbooruGallerySettings.init(this);
         DanbooruGalleryDatabase.init(this);
         Picasso.init(this);
+        NetworkChangeReceiver.init(this);
         SiteAPI.init(this);
         SiteSession.init();
 
