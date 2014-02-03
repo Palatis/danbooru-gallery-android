d1091302129701fe02321888c172bb1ba380c764
Victor Tseng <palatis@gmail.com>
2014-02-03 09:45:38 +0800

    SDK: move IntListPreference to SDK package
    move IntListPreference to the SDK extension package.

--- DanbooruGallery/src/main/java/tw/idv/palatis/danboorugallery/android/preference/IntListPreference.java
+++ DanbooruGallery/src/main/java/tw/idv/palatis/danboorugallery/android/preference/IntListPreference.java
@@ -16,7 +16,7 @@
 //     along with this program. If not, see <http://www.gnu.org/licenses/>
 ////////////////////////////////////////////////////////////////////////////////
 
-package tw.idv.palatis.danboorugallery.preference;
+package tw.idv.palatis.danboorugallery.android.preference;
 
 import android.content.Context;
 import android.preference.ListPreference;
